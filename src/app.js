e6823db41aaf0bbf94d522f8f326fd31aa29257c
F.Gallegos2096 <50385797+GallegosFrancisco2096@users.noreply.github.com>
2022-04-29 15:05:12 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,30 @@
-var types = ["&spades;", "&clubs;", "&hearts;", "&diams;"]
+var types = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+var typesMiddle = ["8", "9", "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7"];
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-let symbol = types[getRandomInt(0, 4)]
-let simbolo = ""
+let symbol = types[getRandomInt(0, 4)];
+let number = typesMiddle[getRandomInt(0, 11)];
+let simbolo = "";
 
 window.onload = () => {
-  document.querySelector('.top-suit').innerHTML = symbol;
-  document.querySelector('.bottom-suit').innerHTML = symbol;
-}
-console.log(symbol)
+  document.querySelector(".bottom-suit").innerHTML = symbol;
+  document.querySelector(".number").innerHTML = number;
+  document.querySelector(".top-suit").innerHTML = symbol;
+};
+console.log(symbol);
 
 if (symbol == "&hearts;") {
-  simbolo = "heart"
-  document.querySelector('.bottom-suit').classList.add(simbolo);
-  document.querySelector('.top-suit').classList.add(simbolo);
+  simbolo = "heart";
+  document.querySelector(".bottom-suit").classList.add(simbolo);
+  document.querySelector(".top-suit").classList.add(simbolo);
 } else if (symbol == "&diams;") {
-  simbolo = "diamont"
-  document.querySelector('.bottom-suit').classList.add(simbolo);
-  document.querySelector('.top-suit').classList.add(simbolo);
-}
-else {
-  document.querySelector('.bottom-suit').classList.add(symbol);
-  document.querySelector('.top-suit').classList.add(symbol);
+  simbolo = "diamont";
+  document.querySelector(".bottom-suit").classList.add(simbolo);
+  document.querySelector(".top-suit").classList.add(simbolo);
+} else {
+  document.querySelector(".bottom-suit").classList.add(symbol);
+  document.querySelector(".top-suit").classList.add(symbol);
 }
